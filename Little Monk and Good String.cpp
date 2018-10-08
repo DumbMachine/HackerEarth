f9099bed6e254a493e61ec01060cf8f43bbf19b0
Ratin_Kumar <ratin.kumar.2k@gmail.com>
2018-10-08 16:16:43 +0530

    Create Little Monk and Good String.cpp

---
+++ Little Monk and Good String.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+bool isvowel(char s){
+    return s=='a' || s=='e' || s=='i' || s=='o'|| s=='u';
+}
+
+int main() {
+  int i;
+	string str;
+  cin>>str;
+  int len_temp;
+  int len_perm;
+  int k=0;
+  for(i=0;i<str.size();i++)
+  {
+    k=i;
+    if(isvowel(str[i]))
+    {
+        len_temp=0;
+      while(isvowel(str[i]) && i<str.size())
+      {
+        i++;
+        len_temp++;
+      }
+    
+    if(len_perm<len_temp){len_perm=len_temp;}
+
+    }
+   i=k;
+  }
+
+cout<<len_perm;
+return 0;
+}
